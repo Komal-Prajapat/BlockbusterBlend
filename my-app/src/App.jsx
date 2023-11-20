c9109802fe8fb0dcff686ba0d46a64b8dd82b974
komalcod <128589948+komalcod@users.noreply.github.com>
2023-11-20 15:35:37 +0530

    movix

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,18 +1,9 @@
-import { useState } from 'react'
-import './App.css'
-import FirstPage from './Components/FirstPage'
-import Navbar from './Components/Navbar'
-import Slide from './Components/Slide'
-import Slide_2 from './Components/Slide_2'
-function App() {
-  
+import React from 'react'
+
+const App = () => {
   return (
     <>
-
-        <Navbar></Navbar>
-        <FirstPage></FirstPage>
-        <Slide ></Slide><br/><br/>
-    <Slide_2></Slide_2>
+      <h1>Lorem, ipsum dolor.</h1> 
     </>
   )
 }
